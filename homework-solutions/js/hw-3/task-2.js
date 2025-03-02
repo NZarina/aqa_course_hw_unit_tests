0d4bf01b45e23ef46038e7e51b96989ba6de7693
unknown <n_zarina@hotmail.com>
2025-03-02 17:29:27 +0300

    Add HW-3

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -1,6 +1,19 @@
+"use strict";
 /*
 
 Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+// Вариант 1:
+const n = 1;
+const sum = Number("" + n) + Number("" + n + n) + Number("" + n + n + n);
+console.log(sum)
+
+// Вариант 2:
+// const n = 1;
+// const nn = String(n) + String(n);
+// const nnn = String(n) + String(n) + String(n);
+// const sum = n + Number(nn) + Number(nnn);
+// console.log(sum);
