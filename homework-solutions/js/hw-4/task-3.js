1aa8f06b524d26f5f375cd8f6b7b6be4149e0d53
unknown <n_zarina@hotmail.com>
2025-03-02 17:46:40 +0300

    Add HW-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 
 (НЕ ОБЯЗАТЕЛЬНО)
@@ -6,3 +7,21 @@
   преобразовываясь в number
 
 */
+
+const minAge = 18;
+const maxAge = 60;
+let age = "61";
+
+if (age != Number(age)) { // если age - не число, то Number(age) = NaN 
+    console.log("Incorrect data type");
+} else {
+    if (age < minAge) {
+        console.log("You don't have access cause your age is " + age + ". It's less then " + minAge + ".");
+    } else if (minAge <= age  && age < maxAge) {
+        console.log("Welcome!");
+    } else if (age > maxAge) {
+        console.log("Keep calm and look Culture channel");
+    } else {
+        console.log("Technical work");
+    } 
+}
